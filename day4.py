29e22dd431e383573afc155bf83b2195aac08127
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-07-04 14:04:17 +0530

    file up

---
+++ day4.py
@@ -0,0 +1,78 @@
+# print("hello world")
+# num1 = input("Plz enter first number : ") 
+# num2 = input("Plz enter second number : ") 
+# print(num1 / num2)   # 
+
+
+# Type casting 
+# int(),float(),str(),dict(),list(),tuple(),set()  
+
+# var = 10.2 
+# var = int(var)
+# print(var)
+# print(type(var))
+
+
+
+# num1 = int(input("Plz enter first number : "))
+# num2 = int(input("Plz enter second number : ")) 
+
+# num1 = int(num1)
+# num2 = int(num2)
+
+# print(num1)
+# print(num2)
+# print(type(num1))
+# print(type(num2))
+
+
+# num1 = int(input("Plz enter first number : "))
+# num2 = int(input("Plz enter second number : ")) 
+
+
+# print(num1 // num2)
+# print(num1 ** num2)
+# print(num1 % num2)
+
+
+
+# num1 = int(input("Plz enter first number : "))
+# num2 = int(input("Plz enter second number : ")) 
+
+# print(num1 == num2)
+# print(num1 != num2)
+# print(num1 < num2)  # 5 < 10 = True
+# print(num1 > num2)  # 55 > 10 = True
+
+# 10 < 9 = False 
+# 10 < 10 = False 
+# 10 <= 10 = True  
+# 10 >= 10 = True 
+
+
+# relative = input("are you relative person of my family press y : ")
+
+# if relative == 'yes':   # True
+#     print("welcome")
+
+# elif relative == "padosi":
+#     print("No need to welcome")
+
+# else:  # False 
+#     print("Bheed kam 😅") 
+
+
+# program 
+
+guest_list = ["mohit","rohit","rinku","syam","harish"]
+name = input("Plz enter your name : ")
+
+if name not in guest_list:
+    print("not present")
+else:
+    print("present")
+
+
+
+
+
---
+++ day5.py
@@ -0,0 +1,20 @@
+### LOGICAL OPERATORS 
+
+# AND 
+
+
+# OR 
+# NOT 
+
+# guest_list = ['rohan','mohan','syam','ritik']
+# freind_list = ['rocky','jack','preet','ravi']
+
+# name = input('Plz enter your name : ')
+# if  name in guest_list or name in freind_list:
+#     print('welcome')
+# else:
+#     print('bheed kam')
+
+# Not operator
+
+print(not False)
