c18bc4051b2aa76ff4dcbfcc47ec8b50d2e0c3d8
Ranjit Singh <78042062+Ranjit-Singh-786@users.noreply.github.com>
2024-07-04 13:06:40 +0530

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,43 @@
+# import manager
+
+
+# print("Employee name : ",manager.employee_name)
+# print("Employee id : ",manager.employee_id)
+# print("Employee password : ",manager.employee_pass)
+# print("Employee salary : ",manager.employee_salary)
+
+
+# import manager as mng
+# print("Employee name : ",mng.employee_name)
+# print("Employee id : ",mng.employee_id)
+# print("Employee password : ",mng.employee_pass)
+# print("Employee salary : ",mng.employee_salary) 
+
+
+# marks = [14,52,63,96,74,52,63,63,42,41,52,63]
+# mng.average_finder(ls=marks)
+
+# import manager    # all content recieved from the another file 
+
+
+
+from manager import employee_id,employee_name,employee_pass,employee_salary
+from manager import manager_name,manager_id,manager_pass
+
+
+print("Employee name : ",employee_name)
+print("Employee id : ",employee_id)
+print("Employee password : ",employee_pass)
+print("Employee salary : ",employee_salary) 
+
+print()
+print(manager_name)
+print(manager_id)
+print(manager_pass)
+
+
+
+
+
+# import manager   # complete source code 
+# from manager import employee_id , employee_name , employee_pass
